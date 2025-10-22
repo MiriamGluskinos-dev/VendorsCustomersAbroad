ddfb483a45944aacea6af892ca0ce7f970e303b9
RutiPos <Rutipos@taxes.gov.il>
2025-10-22 14:01:59 +0300

    change stage url

--- src/environments/environment.stage.ts
+++ src/environments/environment.stage.ts
@@ -1,13 +1,13 @@
 export const environment = {
     production: false,
     configFile: 'assets/settingfolder/setting.json',
-    apiUrl: 'https://shaarolami-query.customs.mof.gov.il/',
+    apiUrl: 'https://shaarolami-query-stage.customs.mof.gov.il/',
     ReCaptcha: {
       siteKey: '6LfBUPArAAAAACmMms_e815NZkI8wsCofMw1a6Nj'
     },
     baseUrls: {
       govscript: '/govscript',
-      shaarolami: '/shaarolami'
+      shaarolami: 'https://shaarolami-query-stage.customs.mof.gov.il/'
     },
      version: 'stage'
   };
