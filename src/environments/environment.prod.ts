eeaf1474edf9d38d61340a00b0cef46eeb1720da
RutiPos <Rutipos@taxes.gov.il>
2025-10-23 08:45:40 +0300

    update env

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  configFile: 'assets/settingfolder/setting.json',
+  apiUrl: 'https://shaarolami-query.customs.mof.gov.il/',
+  ReCaptcha: {
+    siteKey: '6Lc0UPArAAAAAPN-iRRbMwL8Xx_5HebSMAq90iHY'
+  },
+  baseUrls: {
+    govscript: 'https://www.gov.il/',
+    shaarolami: 'https://shaarolami-query.customs.mof.gov.il'
+  }
+}
